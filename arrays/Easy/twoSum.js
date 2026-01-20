683aeb7ec8471c9b0bf20543d913ec447359a4e7
MadhavanNagaraj2024 <madhavannagaraj.dev@gmail.com>
2026-01-20 22:36:51 +0530

    Day 9 - Refactor twoSum function to improve clarity and efficiency

--- arrays/Easy/twoSum.js
+++ arrays/Easy/twoSum.js
@@ -2,17 +2,42 @@
 Output : YES
 Explanation: arr[1] + arr[3] = 14. So, the answer is “YES” for first variant for second variant output will be : [1,3].*/
 
-function twoSum(n, arr, target) {
-  let map = new Map();
+// function twoSum(n, arr, target) {
+//   let map = new Map();
 
-  for (let i = 0; i < n; i++) {
-    let comp = target - arr[i];
+//   for (let i = 0; i < n; i++) {
+//     let comp = target - arr[i];
 
-    if (map.has(comp)) {
-      return [map.get(comp), i];
+//     if (map.has(comp)) {
+//       return [map.get(comp), i];
+//     }
+
+//     map.set(arr[i], i);
+//   }
+
+//   return [-1, -1];
+// }
+
+//optimal approach
+
+function twoSum(arr, target) {
+  let numindexsandvalues = arr.map((val, idx) => [val, idx]);
+
+  numindexsandvalues.sort((a, b) => a[0] - b[0]);
+
+  let left = 0,
+    right = arr.length - 1;
+
+  while (left < right) {
+    let sum = numindexsandvalues[left][0] + numindexsandvalues[right][0];
+
+    if (sum === target) {
+      return [numsWithIndex[left][1], numsWithIndex[right][1]];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right++;
     }
-
-    map.set(arr[i], i);
   }
 
   return [-1, -1];
@@ -22,5 +47,5 @@ const n = 5,
   arr = [2, 6, 5, 8, 11],
   target = 11;
 
-const result = twoSum(n, arr, target);
+const result = twoSum(arr, target);
 console.log("result", result);
