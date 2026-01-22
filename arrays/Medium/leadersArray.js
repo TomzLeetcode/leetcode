7b30108adf25edfb6c1fc473700d430c0bdbbc79
MadhavanNagaraj2024 <madhavannagaraj.dev@gmail.com>
2026-01-22 23:29:56 +0530

    Day 11 - Refactor leadersArray function to improve efficiency and clarity by using a single pass approach

--- arrays/Medium/leadersArray.js
+++ arrays/Medium/leadersArray.js
@@ -7,24 +7,45 @@ Explanation:
  The rightmost element (0) is always a leader.  
 7 and 1 are greater than the elements to their right, making them leaders as well.*/
 
+// Approach 1
+
+// function leadersArray(arr) {
+//   let res = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let leader = true;
+
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] >= arr[i]) {
+//         leader = false;
+//         break;
+//       }
+//     }
+
+//     if (leader) {
+//       res.push(arr[i]);
+//     }
+//   }
+
+//   return res;
+// }
+
+// Approach 2
+
 function leadersArray(arr) {
-  let res = [];
-  for (let i = 0; i < arr.length; i++) {
-    let leader = true;
+  if (arr.length === 0) return [];
 
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] >= arr[i]) {
-        leader = false;
-        break;
-      }
-    }
+  let ans = [];
+  let max = arr[arr.length - 1];
+  ans.push(max);
 
-    if (leader) {
-      res.push(arr[i]);
+  for (let i = arr.length - 2; i >= 0; i--) {
+    if (arr[i] > max) {
+      ans.push(arr[i]);
+      max = arr[i];
     }
   }
 
-  return res;
+  return ans.reverse();
 }
 
 const arr = [4, 7, 1, 0];
